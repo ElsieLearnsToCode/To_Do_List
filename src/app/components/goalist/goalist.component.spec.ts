4c60ef81888ae78edfe7bb508e35ec6802b96a63
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-07 15:57:18 +0300

    add components

---
+++ src/app/components/goalist/goalist.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoalistComponent } from './goalist.component';
+
+describe('GoalistComponent', () => {
+  let component: GoalistComponent;
+  let fixture: ComponentFixture<GoalistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GoalistComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GoalistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/goalist/goalist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goalist',
+  templateUrl: './goalist.component.html',
+  styleUrls: ['./goalist.component.css']
+})
+export class GoalistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
