56f03f23d50dc34efca72d4e3f88e4ad8ffd30d6
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-07 15:01:00 +0300

    initial set-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GoallistComponent } from './goallist/goallist.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  titleCase = 'My First Angular App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoallistComponent } from './goallist/goallist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoallistComponent
   ],
   imports: [
     BrowserModule,
